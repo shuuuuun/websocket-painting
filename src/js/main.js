4b0048b5800f52918be7b596d80e0f19047af42a
motoki-shun <motoki-shun@kayac.com>
2015-11-28 10:54:37 +0900

    main.js読み込みを最後にしてjs整理

--- src/js/main.js
+++ src/js/main.js
@@ -2,57 +2,53 @@
   var ns = win.App = win.App || {};
   
   var util = new ns.Util();
-  var pi = Math.PI;
+  
+  var PI = Math.PI;
+  var COLOR_LIST = ["#FFCC66", "#FF6666", "#CCFF66", "#66CCFF", "#FF6FCF", "#66FFCC", "#FFFF66"];
+  var THROTTLE_INTERVAL = 10;
   
   // var URL = 'http://localhost:2020';
   // var socket = io.connect(URL);
   var socket = io(); // URL指定しなくてもデフォルトで現在のホストに接続する
   
-  var COLOR_LIST = ["#FFCC66", "#FF6666", "#CCFF66", "#66CCFF", "#FF6FCF", "#66FFCC", "#FFFF66"];
-  var throttleInterval = 10;
-  
   var dotAry = [];
   var connectionIdList = [];
   
-  $(function(){
-    setQrCode();
+  var cnvs = document.getElementById("cnvs");
+  var ctx = cnvs.getContext("2d");
+  
+  cnvs.width = $(win).width();
+  cnvs.height = $(win).height();
+  ctx.translate(cnvs.width / 2, cnvs.height / 2);
+  
+  
+  socket.on('createDot', function (data) {
+    dotAry.push(new Dot(data));
+    connectionIdList.push(data.connectionId);
     
-    var cnvs = document.getElementById("cnvs");
-    var ctx = cnvs.getContext("2d");
-    
-    cnvs.width = window.innerWidth;
-    cnvs.height = window.innerHeight;
-    
-    ctx.translate(cnvs.width / 2, cnvs.height / 2);
-    
-    socket.on('createDot', function (data) {
-      dotAry.push(new Dot(data));
-      connectionIdList.push(data.connectionId);
-      
-      socket.on('update', util.throttle(update, throttleInterval));
-    });
-    
-    function update(data){
-      var index = connectionIdList.indexOf(data.connectionId);
-      dotAry[index].update(data);
-      
-      render();
-    }
-    
-    function render(){
-      //ctx.clearRect(0, 0, cnvs.width, cnvs.height);
-      
-      $.each(dotAry, function(index, dot){
-        ctx.save();
-          ctx.fillStyle = dot.fillStyle;
-          ctx.beginPath();
-          ctx.arc(dot.dotX, dot.dotY, 5, 0, pi * 2);
-          ctx.fill();
-          ctx.closePath();
-        ctx.restore();
-      });
-    }
+    socket.on('update', util.throttle(update, THROTTLE_INTERVAL));
   });
+  
+  function update(data){
+    var index = connectionIdList.indexOf(data.connectionId);
+    dotAry[index].update(data);
+    
+    render();
+  }
+  
+  function render(){
+    //ctx.clearRect(0, 0, cnvs.width, cnvs.height);
+    
+    $.each(dotAry, function(index, dot){
+      ctx.save();
+        ctx.fillStyle = dot.fillStyle;
+        ctx.beginPath();
+        ctx.arc(dot.dotX, dot.dotY, 5, 0, PI * 2);
+        ctx.fill();
+        ctx.closePath();
+      ctx.restore();
+    });
+  }
   
   
   function Dot(data){
@@ -68,13 +64,12 @@
   };
   
   
-  function setQrCode(){
+  (function setQrCode(){
     var $qrimg = $(".qr img");
     var size = 200;
     var src = "https://chart.googleapis.com/chart?cht=qr&chs="+size+"x"+size+"&chl=" + encodeURIComponent(location.origin) + "/sp/";
     $qrimg.attr("src", src);
-    console.log(src);
-  }
+  })();
   
   
 })(this, document);
